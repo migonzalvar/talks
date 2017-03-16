fc86dff0d0bddb39c5df96483570612c8aa212c7
Miguel Gonzalez <migonzalvar@gmail.com>
2017-03-16 20:43:52 +0100

    Change to httpbin

--- 2017-03-python-vigo/classic.py
+++ 2017-03-python-vigo/classic.py
@@ -7,27 +7,24 @@ EXCERPT_SIZE = 64
 
 def main():
     print('Entro por aquí')
-    api_key = os.getenv('API_KEY')
-    url = 'http://servizos.meteogalicia.es/apiv3/findPlaces'
+    url = 'http://httpbin.org/get'
     params = dict(
-        API_KEY=api_key,
         location='Vigo',
     )
-    print('Pido url', url)
+    print(f'Pido url {url}')
     r = requests.get(url, params=params)
-    print('Esta es la respuesta:', r.status_code)
-    print(r.text[:EXCERPT_SIZE], '...')
+    print(f'Esta es la respuesta: {r.status_code}')
+    print(r.content)
 
-    url = 'http://servizos.meteogalicia.es/apiv3/getNumericForecastInfo'
+    url = 'http://httpbin.org/status/201'
     params = dict(
-        API_KEY=api_key,
         locationIds=71956,
         variables='temperature',
     )
-    print('Pido url', url)
+    print(f'Pido url {url}')
     r = requests.get(url, params=params)
-    print('Esta es la respuesta:', r.status_code)
-    print(r.text[:EXCERPT_SIZE], '...')
+    print(f'Esta es la respuesta: {r.status_code}')
+    print(r.content)
 
 
 if __name__ == '__main__':
